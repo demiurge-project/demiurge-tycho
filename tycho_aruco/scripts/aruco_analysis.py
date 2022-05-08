4790a25d40af97f2127c29037ec8be6bfc4b35b7
Guillermo Legarda Herranz <glegardah@gmail.com>
2022-05-08 17:54:15 +0200

    Replaces spaces with tabs

--- tycho_aruco/scripts/aruco_analysis.py
+++ tycho_aruco/scripts/aruco_analysis.py
@@ -7,123 +7,123 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def boxplots():
-    """Generate boxplots of marker detection rates from a set of rosbags."""
-    # Scan directory for rosbags and compute mean detection rates for all markers
-    #directory = "/home/trackingsystem/glegarda/rosbags/full_fleet/all"
-    directory = "../rosbags"
-    tmp_means = np.zeros([50, 1])
-    for filename in os.scandir(directory):
-        if ".bag" in filename.name:
-            tmp_means = np.c_[tmp_means, detectionRates(filename)]
-    means = np.transpose(tmp_means[:,1:])
+	"""Generate boxplots of marker detection rates from a set of rosbags."""
+	# Scan directory for rosbags and compute mean detection rates for all markers
+	#directory = "/home/trackingsystem/glegarda/rosbags/full_fleet/all"
+	directory = "../rosbags"
+	tmp_means = np.zeros([50, 1])
+	for filename in os.scandir(directory):
+		if ".bag" in filename.name:
+			tmp_means = np.c_[tmp_means, detectionRates(filename)]
+	means = np.transpose(tmp_means[:,1:])
 
-    # Boxplot
-    fig, axs = plt.subplots(2, 1, figsize=(18, 10), constrained_layout=True)
-    axs[0].set_title("Unsorted")
-    axs[0].set_ylim([0.0, 1.0])
-    axs[0].set_ylabel("Detection rate")
-    axs[0].set_xticks(range(50))
-    axs[0].grid(axis='x')
-    axs[0].set_xlabel("E-puck ID")
-    axs[0].boxplot(means, notch=True, positions=range(50), patch_artist=True, boxprops=dict(facecolor='w'), flierprops=dict(marker='x'))
+	# Boxplot
+	fig, axs = plt.subplots(2, 1, figsize=(18, 10), constrained_layout=True)
+	axs[0].set_title("Unsorted")
+	axs[0].set_ylim([0.0, 1.0])
+	axs[0].set_ylabel("Detection rate")
+	axs[0].set_xticks(range(50))
+	axs[0].grid(axis='x')
+	axs[0].set_xlabel("E-puck ID")
+	axs[0].boxplot(means, notch=True, positions=range(50), patch_artist=True, boxprops=dict(facecolor='w'), flierprops=dict(marker='x'))
 
-    # Sort in descending order and redo boxplot
-    medians = np.median(means, axis=0)
-    positions = np.zeros(50, dtype=int)
-    labels = np.zeros(50, dtype=int)
-    for i in range(50):
-        index = np.argmax(medians)
-        medians[index] = -1
-        positions[index] = i
-        labels[i] = index
+	# Sort in descending order and redo boxplot
+	medians = np.median(means, axis=0)
+	positions = np.zeros(50, dtype=int)
+	labels = np.zeros(50, dtype=int)
+	for i in range(50):
+		index = np.argmax(medians)
+		medians[index] = -1
+		positions[index] = i
+		labels[i] = index
 
-    axs[1].set_title("Sorted by descending median")
-    axs[1].set_ylim([0.0, 1.0])
-    axs[1].set_ylabel("Detection rate")
-    axs[1].grid(axis='x')
-    axs[1].set_xlabel("E-puck ID")
-    axs[1].boxplot(means, notch=True, positions=positions, patch_artist=True, boxprops=dict(facecolor='w'), flierprops=dict(marker='x'))
-    axs[1].set_xticks(range(50))
-    axs[1].set_xticklabels(labels)
-    plt.savefig("../rosbags/epuck_detection_rates.pdf")
+	axs[1].set_title("Sorted by descending median")
+	axs[1].set_ylim([0.0, 1.0])
+	axs[1].set_ylabel("Detection rate")
+	axs[1].grid(axis='x')
+	axs[1].set_xlabel("E-puck ID")
+	axs[1].boxplot(means, notch=True, positions=positions, patch_artist=True, boxprops=dict(facecolor='w'), flierprops=dict(marker='x'))
+	axs[1].set_xticks(range(50))
+	axs[1].set_xticklabels(labels)
+	plt.savefig("../rosbags/epuck_detection_rates.pdf")
 
 
 
 def detectionRates(filename):
-    """Determine which markers are detected most often."""
-    bag = rosbag.Bag(filename.path)
-    types_and_topics = bag.get_type_and_topic_info()
-    n_epucks = 50
+	"""Determine which markers are detected most often."""
+	bag = rosbag.Bag(filename.path)
+	types_and_topics = bag.get_type_and_topic_info()
+	n_epucks = 50
 
-    # Get number of messages published by each camera
-    n_cam0, n_cam1,  n_cam2 = 0, 0, 0
-    for key, value in types_and_topics.topics.items():
-        n_msg = value.message_count
-        if key == "/camera_0/tracker/positions_stamped":
-            n_cam0 = n_msg
-        elif key == "/camera_1/tracker/positions_stamped":
-            n_cam1 = n_msg
-        elif key == "/camera_2/tracker/positions_stamped":
-            n_cam2 = n_msg
+	# Get number of messages published by each camera
+	n_cam0, n_cam1,  n_cam2 = 0, 0, 0
+	for key, value in types_and_topics.topics.items():
+		n_msg = value.message_count
+		if key == "/camera_0/tracker/positions_stamped":
+			n_cam0 = n_msg
+		elif key == "/camera_1/tracker/positions_stamped":
+			n_cam1 = n_msg
+		elif key == "/camera_2/tracker/positions_stamped":
+			n_cam2 = n_msg
 
-    # Calculate rate of marker detection for each marker and camera
-    m_cam0 = np.zeros([n_epucks, 1])
-    m_cam1 = np.zeros([n_epucks, 1])
-    m_cam2 = np.zeros([n_epucks, 1])
-    for key, value in sorted(types_and_topics.topics.items()):
-        if "epuck" in key:
-            slash_pos = key.rfind("/")
-            n_msg = value.message_count
-            if "camera_0" in key:
-                epuck = int(key[16:slash_pos])
-                m_cam0[epuck] = n_msg / n_cam0
-            elif "camera_1" in key:
-                epuck = int(key[16:slash_pos])
-                m_cam1[epuck] = n_msg / n_cam1
-            elif "camera_2" in key:
-                epuck = int(key[16:slash_pos])
-                m_cam2[epuck] = n_msg / n_cam2
+	# Calculate rate of marker detection for each marker and camera
+	m_cam0 = np.zeros([n_epucks, 1])
+	m_cam1 = np.zeros([n_epucks, 1])
+	m_cam2 = np.zeros([n_epucks, 1])
+	for key, value in sorted(types_and_topics.topics.items()):
+		if "epuck" in key:
+			slash_pos = key.rfind("/")
+			n_msg = value.message_count
+			if "camera_0" in key:
+				epuck = int(key[16:slash_pos])
+				m_cam0[epuck] = n_msg / n_cam0
+			elif "camera_1" in key:
+				epuck = int(key[16:slash_pos])
+				m_cam1[epuck] = n_msg / n_cam1
+			elif "camera_2" in key:
+				epuck = int(key[16:slash_pos])
+				m_cam2[epuck] = n_msg / n_cam2
 
-    # Mean detection rates
-    m_cam_all = np.zeros([n_epucks, 1])
-    for i in range(n_epucks):
-        m_cam_all[i] = np.mean(np.array([m_cam0[i], m_cam1[i], m_cam2[i]]))
+	# Mean detection rates
+	m_cam_all = np.zeros([n_epucks, 1])
+	for i in range(n_epucks):
+		m_cam_all[i] = np.mean(np.array([m_cam0[i], m_cam1[i], m_cam2[i]]))
 
-    # Recommended markers
-    mean_rates = {}
-    for i in range(n_epucks):
-        mean_rates[i] = m_cam_all[i, 0]
+	# Recommended markers
+	mean_rates = {}
+	for i in range(n_epucks):
+		mean_rates[i] = m_cam_all[i, 0]
 
-    # print("Recommended markers:")
-    # print("ID    Rate")
-    # it = 0
-    # for key, value in sorted(mean_rates.items(), key=lambda item: item[1], reverse=True):
-    #     print("{0:2}    {1}".format(key, value))
-    #     it = it + 1
-    #     if it == 20:
-    #         print("--------------------")
+	# print("Recommended markers:")
+	# print("ID	Rate")
+	# it = 0
+	# for key, value in sorted(mean_rates.items(), key=lambda item: item[1], reverse=True):
+	# 	print("{0:2}    {1}".format(key, value))
+	# 	it = it + 1
+	# 	if it == 20:
+	# 		print("--------------------")
 
-    # Plot results
-    x_epuck = range(n_epucks)
-    plt.figure(figsize=(15, 4.8))
-    plt.scatter(x_epuck, m_cam0, marker='x', label="Camera 0")
-    plt.scatter(x_epuck, m_cam1, marker='x', label="Camera 1")
-    plt.scatter(x_epuck, m_cam2, marker='x', label="Camera 2")
-    plt.plot(x_epuck, m_cam_all, 'ko--', label="Mean")
-    plt.legend(loc='best')
-    plt.ylim([0.0, 1.0])
-    plt.ylabel("Detection rate")
-    plt.xticks(range(n_epucks))
-    plt.grid(axis='x')
-    plt.xlabel("E-puck ID")
-    plt.savefig(filename.path + ".pdf")
+	# Plot results
+	x_epuck = range(n_epucks)
+	plt.figure(figsize=(15, 4.8))
+	plt.scatter(x_epuck, m_cam0, marker='x', label="Camera 0")
+	plt.scatter(x_epuck, m_cam1, marker='x', label="Camera 1")
+	plt.scatter(x_epuck, m_cam2, marker='x', label="Camera 2")
+	plt.plot(x_epuck, m_cam_all, 'ko--', label="Mean")
+	plt.legend(loc='best')
+	plt.ylim([0.0, 1.0])
+	plt.ylabel("Detection rate")
+	plt.xticks(range(n_epucks))
+	plt.grid(axis='x')
+	plt.xlabel("E-puck ID")
+	plt.savefig(filename.path + ".pdf")
 
-    bag.close()
+	bag.close()
 
-    return m_cam_all
+	return m_cam_all
 
 if __name__ == '__main__':
-    try:
-        boxplots()
-    except rospy.ROSInterruptException:
-        pass
+	try:
+		boxplots()
+	except rospy.ROSInterruptException:
+		pass
--- tycho_tracker/src/tracker_node.cpp
+++ tycho_tracker/src/tracker_node.cpp
@@ -11,85 +11,85 @@ static const std::string OPENCV_WINDOW = "Image window";
 
 class Tracker
 {
-    ros::NodeHandle nh_;
-    image_transport::ImageTransport it_;
-    image_transport::Subscriber image_sub_;
-    ros::Publisher pub;
-    cv::Mat camera_matrix, dist_coeff; // These can be set after camera calibration to undistort image
-    cv::Ptr<cv::aruco::DetectorParameters> parameters;
-    cv::Ptr<cv::aruco::Dictionary> dictionary;
+	ros::NodeHandle nh_;
+	image_transport::ImageTransport it_;
+	image_transport::Subscriber image_sub_;
+	ros::Publisher pub;
+	cv::Mat camera_matrix, dist_coeff; // These can be set after camera calibration to undistort image
+	cv::Ptr<cv::aruco::DetectorParameters> parameters;
+	cv::Ptr<cv::aruco::Dictionary> dictionary;
 
 public:
-    Tracker()
-        : it_(nh_)
-    {
-        // Subscribe to camera
-        bool crop;
-        nh_.getParam("tracker/crop", crop);
-        if (crop)
-        {
-            image_sub_ = it_.subscribe("image_cropped", 20, &Tracker::imageCb, this);
-        }
-        else
-        {
-            image_sub_ = it_.subscribe("camera/image_rect_color", 20, &Tracker::imageCb, this);
-        }
-        
-        // Advertise a list of integers
-        pub = nh_.advertise<std_msgs::UInt32MultiArray>("tracker/positions_stamped", 20);
+	Tracker()
+		: it_(nh_)
+	{
+		// Subscribe to camera
+		bool crop;
+		nh_.getParam("tracker/crop", crop);
+		if (crop)
+		{
+			image_sub_ = it_.subscribe("image_cropped", 20, &Tracker::imageCb, this);
+		}
+		else
+		{
+			image_sub_ = it_.subscribe("camera/image_rect_color", 20, &Tracker::imageCb, this);
+		}
+		
+		// Advertise a list of integers
+		pub = nh_.advertise<std_msgs::UInt32MultiArray>("tracker/positions_stamped", 20);
 
-        // Set aruco dictionary to use for marker detection
-        dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_4X4_50);
+		// Set aruco dictionary to use for marker detection
+		dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_4X4_50);
 
-        parameters = cv::aruco::DetectorParameters::create();
-    }
+		parameters = cv::aruco::DetectorParameters::create();
+	}
 
-    // Subscriber callback function
-    void imageCb(const sensor_msgs::ImageConstPtr& msg)
-    {
-        cv_bridge::CvImagePtr cv_ptr;
-        try
-        {
-            cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
-        }
-        catch (cv_bridge::Exception& e)
-        {
-            ROS_ERROR("cv_bridge exception: %s", e.what());
-        }
+	// Subscriber callback function
+	void imageCb(const sensor_msgs::ImageConstPtr& msg)
+	{
+		cv_bridge::CvImagePtr cv_ptr;
+		try
+		{
+			cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+		}
+		catch (cv_bridge::Exception& e)
+		{
+			ROS_ERROR("cv_bridge exception: %s", e.what());
+		}
 
-        // Get time stamp
-        ros::Time stamp = cv_ptr->header.stamp;
+		// Get time stamp
+		ros::Time stamp = cv_ptr->header.stamp;
 
-        // List that will store detection results
-        std::vector<int> ids;
-        std::vector<std::vector<cv::Point2f> > corners;
+		// List that will store detection results
+		std::vector<int> ids;
+		std::vector<std::vector<cv::Point2f> > corners;
 
-        // Aruco detection function
-        cv::aruco::detectMarkers(cv_ptr->image, dictionary, corners, ids, parameters);
+		// Aruco detection function
+		cv::aruco::detectMarkers(cv_ptr->image, dictionary, corners, ids, parameters);
 
-        // Add time stamp and all marker positions to list
-        // std_msgs::UInt32MultiArray has no header, so stamp is introduced in the first two entries: (sec, nsec)
-        std_msgs::UInt32MultiArray positions_stamped;
-        positions_stamped.data.push_back(stamp.sec);
-        positions_stamped.data.push_back(stamp.nsec);
-        for (int i = 0; i < corners.size(); i++)
-        {
-            positions_stamped.data.push_back(ids.at(i));
-            for (int j = 0; j < corners.at(i).size(); j++)
-            {
-                positions_stamped.data.push_back(corners.at(i).at(j).x);
-                positions_stamped.data.push_back(corners.at(i).at(j).y);
-            }
-        }
-        // Publish
-        pub.publish(positions_stamped);
-    }
+		// Add time stamp and all marker positions to list
+		// std_msgs::UInt32MultiArray has no header, so stamp is introduced in the first two entries: (sec, nsec)
+		std_msgs::UInt32MultiArray positions_stamped;
+		positions_stamped.data.push_back(stamp.sec);
+		positions_stamped.data.push_back(stamp.nsec);
+		for (int i = 0; i < corners.size(); i++)
+		{
+			positions_stamped.data.push_back(ids.at(i));
+			for (int j = 0; j < corners.at(i).size(); j++)
+			{
+				positions_stamped.data.push_back(corners.at(i).at(j).x);
+				positions_stamped.data.push_back(corners.at(i).at(j).y);
+			}
+		}
+		// Publish
+		pub.publish(positions_stamped);
+	}
 };
 
 int main(int argc, char** argv)
 {
-    ros::init(argc, argv, "tracker");
-    Tracker tracker_object;
-    ros::spin();
-    return 0;
+	ros::init(argc, argv, "tracker");
+	Tracker tracker_object;
+	ros::spin();
+	return 0;
 }
